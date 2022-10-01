510fa27f0882ae797cb2e2c2395928d88dc88202
Leo <89736703+NexWan@users.noreply.github.com>
2022-10-01 05:10:29 -0500

    Add files via upload

---
+++ r6scriptz/main.py
@@ -0,0 +1,20 @@
+import time
+import keyboard
+import pyautogui as pg
+import random as ran
+
+file = open("config.txt","r")
+start = file.readline().splitlines()
+end = file.readline().splitlines()
+
+print(start)
+print(end)
+
+while True:
+    if keyboard.is_pressed(start):
+        while True:
+            x = ran.randint(0, 1000)
+            y = ran.randint(0, 750)
+            pg.moveTo(x, y, 0)
+            if keyboard.is_pressed(end):
+                quit()
